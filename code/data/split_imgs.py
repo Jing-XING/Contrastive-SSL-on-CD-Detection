87751468afa086429fde1a52279c2eb874d50394
jing <xjjsdsb@qq.com>
2022-05-02 23:56:56 +0200

    update

--- code/data/split_imgs.py
+++ code/data/split_imgs.py
@@ -6,11 +6,14 @@ PS: if the file is .gvf files, use command like 'ffmpeg -i .gvf ./E/%d.png'to ex
 import os
 import shutil
 imgs_path = '../../dataset/DataCrohnIPI/val/imgs'
+save_path= '../../dataset/DataCrohnIPI/val/imgs_split_by_folds'
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 csv_file = "../../dataset/DataCrohnIPI/val/CrohnIPI_description.csv"
-imgs_split_by_labels_7 = "imgs_split_by_labels_7"
-imgs_split_by_labels_2 = "imgs_split_by_labels_2"
+# imgs_split_by_labels_7 = "imgs_split_by_labels_7"
+# imgs_split_by_labels_2 = "imgs_split_by_labels_2"
 
-imgs_split_by_folds = "imgs_split_by_folds"
+# imgs_split_by_folds = "imgs_split_by_folds"
 with open("../../dataset/DataCrohnIPI/val/CrohnIPI_description.csv") as f:
     for line in f:
         name,label,fold_num=line.strip().split(',')
@@ -18,7 +21,7 @@ with open("../../dataset/DataCrohnIPI/val/CrohnIPI_description.csv") as f:
             label='U_bigger_than_10'
         if label!='Label':
             img_path = os.path.join(imgs_path,name)
-            label_path=os.path.join(imgs_split_by_labels_7, label)
+            # label_path=os.path.join(imgs_split_by_labels_7, label)
 
             # if not os.path.exists(label_path):
             #     os.mkdir(label_path)
@@ -32,15 +35,17 @@ with open("../../dataset/DataCrohnIPI/val/CrohnIPI_description.csv") as f:
             for n in range(1,6):
                 if label=='N':
                     if fold_num==str(n):
-                        new_imgs_labels_path2 = os.path.join(imgs_split_by_labels_2,'fold1','N',name)
+                        new_path = os.path.join(save_path,f'fold{n}','N')
                     else:
-                        new_imgs_labels_path2 = os.path.join(imgs_split_by_labels_2,'notfold1','N',name)
+                        new_path = os.path.join(save_path,f'notfold{n}','N')
                 else:
                     if fold_num==str(n):
-                        new_imgs_labels_path2 = os.path.join(imgs_split_by_labels_2,'fold1','ABN',name)
+                        new_path = os.path.join(save_path,f'fold{n}','ABN')
                     else:
-                        new_imgs_labels_path2 = os.path.join(imgs_split_by_labels_2,'notfold1','ABN',name)
-                shutil.copyfile(img_path, new_imgs_labels_path2)
+                        new_path = os.path.join(save_path,f'notfold{n}','ABN')
+                if not os.path.exists(new_path):
+                    os.makedirs(new_path)
+                shutil.copyfile(img_path, new_path+'/'+name)
 
 
 
