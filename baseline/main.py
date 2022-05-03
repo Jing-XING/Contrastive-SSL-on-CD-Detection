d6fb672f598a3b723967ccfb6dea8c1622f64b5e
nantes_sever <3240333103@qq.com>
2022-05-03 17:19:20 +0200

    optimize the log for baseline

--- baseline/main.py
+++ baseline/main.py
@@ -15,47 +15,7 @@ def Model(nb_classes, pretrained=False):
         base_resnet34 = resnet34(pretrained=True)
         model.load_state_dict(base_resnet34.state_dict())
     model.fc = nn.Linear(512, nb_classes)
-
     return model
-mode = 'train'
-foldnum=1
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-ckpt_path= os.path.join('ckpt','fold'+str(foldnum)+'.pth')
-# test_dir = "/shareData3/lab-xing.jing/project/nantes/imgs_split_by_labels_2/fold1/"
-# train_dir = "/shareData3/lab-xing.jing/project/nantes/imgs_split_by_labels_2/notfold1/"
-
-test_dir = "/opt/data/home-jing/SSL_for_CD/dataset/val/fold1"
-train_dir = "/opt/data/home-jing/SSL_for_CD/dataset/val/notfold1"
-batch_size = 16
-log_path = './log'
-lr = 0.001
-now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
-log_path = os.path.join(log_path,now)
-if not os.path.exists(log_path):
-    os.mkdir(log_path)
-writer=SummaryWriter(log_path)
-
-train_augs = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
-test_augs = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-])
-
-
-train_set = datasets.ImageFolder(train_dir, transform=train_augs)
-test_set = datasets.ImageFolder(test_dir, transform=test_augs)
-print('len train set/test_set',len(train_set),len(test_set))
-
-
-train_iter = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-test_iter = DataLoader(test_set, batch_size=batch_size)
-
-
 def train(net, train_iter, test_iter, criterion, optimizer, num_epochs):
     net = net.to(device)
     print("training on", device)
@@ -75,7 +35,7 @@ def train(net, train_iter, test_iter, criterion, optimizer, num_epochs):
             train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
             n += y.shape[0]
             batch_count += 1
-        if epoch % 2 == 0:
+        if epoch % 1 == 0:
             with torch.no_grad():
                 net.eval()
                 test_acc_sum, test_loss_sum, n2, batch_count2 = 0.0, 0.0, 0, 0
@@ -90,23 +50,87 @@ def train(net, train_iter, test_iter, criterion, optimizer, num_epochs):
                     batch_count2 += 1
             if best_accuracy < (test_acc_sum / n2):
                 torch.save(net.state_dict(), ckpt_path)
-            writer.add_scalar('loss/val_loss', test_loss_sum / batch_count2, epoch)
-            writer.add_scalar('accuracy/val_acc', test_acc_sum / n2, epoch)
-        writer.add_scalar('loss/train_loss', train_loss_sum / batch_count, epoch)
-        writer.add_scalar('accuracy/train_acc', train_acc_sum / n, epoch)
+                best_accuracy = test_acc_sum / n2
+            val_writer.add_scalar('loss', test_loss_sum / batch_count2, epoch)
+            val_writer.add_scalar('accuracy', test_acc_sum / n2, epoch)
+        train_writer.add_scalar('loss', train_loss_sum / batch_count, epoch)
+        train_writer.add_scalar('accuracy', train_acc_sum / n, epoch)
 
 
         print('epoch %d, train loss %.4f, train acc %.3f, test loss %.4f, test acc %.3f, best acc %.3f, time %.1f sec'
-              % (epoch + 1, train_loss_sum / batch_count, train_acc_sum / n, test_loss_sum / batch_count2, test_acc_sum / n2, best_accuracy, time.time() - start))
+              % (epoch + 1, train_loss_sum / batch_count, train_acc_sum / n, test_loss_sum / batch_count2, test_acc_sum / n2,
+                 best_accuracy, time.time() - start))
+        
+mode = 'train'
+foldnum=1
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# test_dir = "/shareData3/lab-xing.jing/project/nantes/imgs_split_by_labels_2/fold1/"
+# train_dir = "/shareData3/lab-xing.jing/project/nantes/imgs_split_by_labels_2/notfold1/"
+num_epochs=700
+test_dir = f"../dataset/val/imgs_split_by_folds/fold{foldnum}"
+train_dir = f"../dataset/val/imgs_split_by_folds/notfold{foldnum}"
+batch_size = 16
+log_path = './log'
+lr = 3e-4
+now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+log_path = os.path.join(log_path,'fold'+str(foldnum),now)
+ckpt_path= os.path.join('ckpt','fold'+str(foldnum))
+if not os.path.exists(ckpt_path):
+    os.makedirs(ckpt_path)
+ckpt_path= os.path.join(ckpt_path, now+'.pth')
+train_log_path = os.path.join(log_path, 'train')
+val_log_path = os.path.join(log_path, 'val')
+if not os.path.exists(val_log_path):
+    os.makedirs(val_log_path)
+if not os.path.exists(train_log_path):
+    os.makedirs(train_log_path) 
+train_writer = SummaryWriter(train_log_path)
+val_writer = SummaryWriter(val_log_path)
+
+# train_augs = transforms.Compose([
+#     transforms.RandomResizedCrop(224),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.ToTensor(),
+# ])
+# test_augs = transforms.Compose([
+#     transforms.Resize(256),
+#     transforms.CenterCrop(224),
+#     transforms.ToTensor(),
+# ])
+train_augs = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation((180, 180)),
+            transforms.Resize(256),
+
+            transforms.ToTensor()
+        ])
+test_augs = transforms.Compose([
+    transforms.Resize(256),
+    transforms.ToTensor()
+])
+
+train_set = datasets.ImageFolder(train_dir, transform=train_augs)
+test_set = datasets.ImageFolder(test_dir, transform=test_augs)
+print('len train set/test_set',len(train_set),len(test_set))
+
+
+train_iter = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_iter = DataLoader(test_set, batch_size=batch_size)
+
+
+
 
 
 
 pretrained_net = Model(nb_classes=2, pretrained=True)
 output_params = list(map(id, pretrained_net.fc.parameters()))
 feature_params = filter(lambda p: id(p) not in output_params, pretrained_net.parameters())
-optimizer = optim.SGD([{'params': feature_params},
-                       {'params': pretrained_net.fc.parameters(), 'lr': lr * 10}],
-                      lr=lr, momentum=0.9)
+# optimizer = optim.SGD([{'params': feature_params},
+#                        {'params': pretrained_net.fc.parameters(), 'lr': lr * 10}],
+#                       lr=lr, momentum=0.9)
+optimizer = optim.Adam(pretrained_net.parameters(), lr=lr)
 
 loss = torch.nn.CrossEntropyLoss()
-train(pretrained_net, train_iter, test_iter, loss, optimizer, num_epochs=300)
+
+train(pretrained_net, train_iter, test_iter, loss, optimizer, num_epochs=num_epochs)
---
+++ code/data/split_imgs.py
@@ -0,0 +1,52 @@
+'''
+split labeled images according to 7 labels(7 symptom);2 labels(norman('N') and abnormal('ABN');5 folders(cross-validation)
+
+PS: if the file is .gvf files, use command like 'ffmpeg -i .gvf ./E/%d.png'to extract png frame by frame.
+'''
+import os
+import shutil
+imgs_path = '../../dataset/val/labeled'
+save_path= '../../dataset/val/imgs_split_by_folds'
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+csv_file = "./CrohnIPI_description.csv"
+# imgs_split_by_labels_7 = "imgs_split_by_labels_7"
+# imgs_split_by_labels_2 = "imgs_split_by_labels_2"
+
+# imgs_split_by_folds = "imgs_split_by_folds"
+with open(csv_file) as f:
+    for line in f:
+        name,label,fold_num=line.strip().split(',')
+        if label=='U>10':
+            label='U_bigger_than_10'
+        if label!='Label':
+            img_path = os.path.join(imgs_path,name)
+            # label_path=os.path.join(imgs_split_by_labels_7, label)
+
+            # if not os.path.exists(label_path):
+            #     os.mkdir(label_path)
+            # new_imgs_labels_path=os.path.join(label_path,name)
+            # fold_path = os.path.join(imgs_split_by_folds,fold_num)
+            # if not os.path.exists(fold_path):
+            #     os.mkdir(fold_path)
+            # new_imgs_folds_path = os.path.join(fold_path,name)
+            # shutil.copyfile(img_path, new_imgs_labels_path)
+            # shutil.copyfile(img_path, new_imgs_folds_path)
+            for n in range(1,6):
+                if label=='N':
+                    if fold_num==str(n):
+                        new_path = os.path.join(save_path,f'fold{n}','N')
+                    else:
+                        new_path = os.path.join(save_path,f'notfold{n}','N')
+                else:
+                    if fold_num==str(n):
+                        new_path = os.path.join(save_path,f'fold{n}','ABN')
+                    else:
+                        new_path = os.path.join(save_path,f'notfold{n}','ABN')
+                if not os.path.exists(new_path):
+                    os.makedirs(new_path)
+                shutil.copyfile(img_path, new_path+'/'+name)
+
+
+
+
